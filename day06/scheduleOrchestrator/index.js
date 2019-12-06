6e3c231e3f030fdcf53e516d16de4fa7cf28617c
Niall Kelly <nkelly75@gmail.com>
2019-12-06 15:22:10 +0000

    initial commit from christian's dev.to article

---
+++ day06/scheduleOrchestrator/index.js
@@ -0,0 +1,15 @@
+﻿const df = require("durable-functions");
+
+/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ */
+
+ module.exports = df.orchestrator(function* (context) {
+    const input = context.df.getInput();
+
+    // TODO -- 1
+    yield context.df.createTimer(new Date(input.startAt))
+  
+    return yield context.df.callActivity('sendMail', input);
+});
---
+++ day06/sendMail/index.js
@@ -0,0 +1,20 @@
+﻿const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env['SENDGRID_API_KEY']);
+
+/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ */
+module.exports = async function (context) {
+    const { email, title, startAt, description } = context.bindings.payload;
+
+    const msg = {
+        to: email,
+        from: { email: 'nkelly75@gmail.com', name: 'Durable Functions' },
+        subject: `Test: ${title}`,
+        html: `<h4>${title} @ ${startAt}</h4> <p>${description}</p>`
+    };
+    sgMail.send(msg);
+
+    return msg;
+};
