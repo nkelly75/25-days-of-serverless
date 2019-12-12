aa097369504f8c7008a51c10be639a9773d07d98
Niall Kelly <nkelly75@gmail.com>
2019-12-12 23:18:36 +0000

    caching in place

--- day12/Card/index.js
+++ day12/Card/index.js
@@ -1,29 +1,36 @@
+const redis = require("redis");
+const bluebird = require("bluebird");
 const Gists = require('gists');
 const gists = new Gists({});
 const showdown = require('showdown');
 const emoji = require('../shared/emoji');
 
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+const client = redis.createClient(6380,
+    process.env.REDISCACHEHOSTNAME, {
+    auth_pass: process.env.REDISCACHEKEY,
+    tls: { servername: process.env.REDISCACHEHOSTNAME }
+});
+
+let converter = null;
+
 module.exports = async function (context, req) {
     context.log('Retrieving a card');
 
-    await emoji.getSupportedEmojis();
-
-    const converter = new showdown.Converter({
-        extensions: [emoji.convertEmojis]
-    });
-
     if (req.params.id) {
-        let html = null;
-        let res = await gists.get(req.params.id);
+        let id = req.params.id;
 
-        if (res.body && res.body.files) {
-            let fileEntries = Object.entries(res.body.files);
-            if (fileEntries.length > 0) {
-                context.log('Filename: ' + fileEntries[0][0]);
-                let fileDetails = fileEntries[0][1];
-                if (fileDetails.content) {
-                    html = converter.makeHtml(fileDetails.content);
-                }
+        // Check redis
+        var html = await client.getAsync(id);
+
+        if (!html) {
+            // Not cached, try to retrieve from gist, render etc.
+            html = await retrieveAndRender(id, context);
+            if (html) {
+                // Save for next time
+                await client.setAsync(id, html);
             }
         }
 
@@ -38,7 +45,7 @@ module.exports = async function (context, req) {
             context.res = {
                 status: 500,
                 body: "Internal error"
-            };
+            };    
         }
     } else {
         context.res = {
@@ -47,3 +54,30 @@ module.exports = async function (context, req) {
         };
     }
 };
+
+retrieveAndRender = async function (id, context) {
+    // Get map of emoji names to image links from github API (may already have it)
+    await emoji.getSupportedEmojis();
+
+    if (!converter) {
+        converter = new showdown.Converter({
+            extensions: [emoji.convertEmojis]
+        });
+    }
+
+    let html = null;
+    let res = await gists.get(id);
+
+    if (res.body && res.body.files) {
+        let fileEntries = Object.entries(res.body.files);
+        if (fileEntries.length > 0) {
+            context.log('Filename: ' + fileEntries[0][0]);
+            let fileDetails = fileEntries[0][1];
+            if (fileDetails.content) {
+                html = converter.makeHtml(fileDetails.content);
+            }
+        }
+    }
+
+    return html;
+}
